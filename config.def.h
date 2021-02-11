d8175b97c2f7f5b2eb6d308892c179ce724a561a
usrname <thenewmailingadress@gmail.com>
2021-02-11 14:28:05 -0600

    2-11-21

--- config.def.h
+++ config.def.h
@@ -1,23 +1,35 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;       /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;     /* -c option; centers dmenu on screen */
+static int min_width = 500;  /* minimum width when centered */
+static int fuzzy = 1;        /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Hack:pixelsize=13:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=13:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#FFFFFF", "#000000" },
+	[SchemeSel] = { "#FFFFFF", "#000000" },
+	[SchemeSelHighlight] = { "#FFFFFF", "#000000" },
+	[SchemeNormHighlight] = { "#FFFFFF", "#000000" },
+	[SchemeOut] = { "#FFFFFF", "#000000" },
+	[SchemeMid] = { "#FFFFFF", "#000000" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
+static unsigned int lineheight = 22;  /* -h option; minimum height of a menu line */
+static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;  /* -bw option; to add border width */
