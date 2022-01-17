ef85e92d9a08bc30c62a01099410b6a257cf83f0
usrname <thenewmailingadress@gmail.com>
2022-01-16 20:34:32 -0600

    update dmenu to 5.0

--- config.h
+++ config.h
@@ -6,10 +6,11 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Comic Mono:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 #import "/home/adrian/.cache/wal/colors-wal-dmenu.h"
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
@@ -20,4 +21,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static const unsigned int border_width = 5;
