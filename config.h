8252fddb6bac6e3e392fc5550fac675a66366180
adrian <thenewmailingadress@gmail.com>
2022-06-08 13:19:26 -0500

    06-08-2022

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Comic Mono:size=10"
+	"Hack:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 #import "/home/adrian/.cache/wal/colors-wal-dmenu.h"
